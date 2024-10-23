ec09dbc6a5d0cb606c4d191366db4a934faf0409
JRSkates <skatesproduction@gmail.com>
2024-10-23 14:07:55 +0100

    Employee class created

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,8 +1,32 @@
 class Employee {
+    #salary
+    #isHired
+    constructor(name, position, salary) {
+        this.name = name;
+        this.position = position;
+        this.#salary = salary;
+        this.#isHired = true;
+    }
 
+    getSalary() {
+        return this.#salary;
+    }
 
-
+    setSalary(salary) {
+        this.#salary = salary;
+    }
     
+    getStatus() {
+        return this.#isHired;
+    }
+
+    setStatus(command) {
+        if (command === "fire") {
+            this.#isHired = false;
+        } else {
+            this.#isHired = true;
+        }
+    }
 }
 
 module.exports = {

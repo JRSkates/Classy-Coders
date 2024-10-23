49bc9cc5041f26535ab5193bc755b3385db0d97d
JRSkates <skatesproduction@gmail.com>
2024-10-23 14:16:50 +0100

    Manager Class Created

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,8 +1,24 @@
-class Manager {
+const { Employee } = require("./Employee")
 
+class Manager extends Employee {
+    #employeesManaged
+    constructor(name, position, salary, department) {
+        super(name, position, salary)
+        this.department = department;
+        this.#employeesManaged = [];
+    }
 
-
+    getEmployeesManaged() {
+        return this.#employeesManaged;
+    }
     
+    setEmployeesManaged(employee) {
+        if (employee instanceof Employee) {
+            this.#employeesManaged.push(employee);
+        } else {
+            throw new Error('Invalid employee object');  // Handle invalid input
+        }
+    }
 }
 
 module.exports = {
